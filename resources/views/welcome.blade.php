a86a492e4d92e398f105a24579338dc07c005032
Ahmad Saiful Bahri <epool862@gmail.com>
2023-12-22 00:49:42 +0800

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Kalkulator ASB 2023 vs 2022 - by Ahmad Saiful Bahri</title>
 
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
